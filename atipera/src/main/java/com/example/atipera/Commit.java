79d8eea033374d4b175f5c981847314cfda72fec
Kajojan <kojoty1999@gmail.com>
2023-09-18 16:23:36 +0200

    Commit and Login class

---
+++ atipera/src/main/java/com/example/atipera/Commit.java
@@ -0,0 +1,14 @@
+package com.example.atipera;
+
+public class Commit {
+    private String sha;
+
+    public String getSha() {
+        return sha;
+    }
+
+    public void setSha(String sha) {
+        this.sha = sha;
+    }
+
+}
---
+++ atipera/src/main/java/com/example/atipera/Login.java
@@ -0,0 +1,13 @@
+package com.example.atipera;
+
+public class Login {
+        private String login;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
