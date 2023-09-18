7b2f3b201a8a070d4ae9254e0b9c40b0edb59c5d
Kajojan <kojoty1999@gmail.com>
2023-09-18 16:19:36 +0200

    getUserRepositories func

---
+++ atipera/src/main/java/com/example/atipera/githubServices.java
@@ -0,0 +1,49 @@
+package com.example.atipera;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+
+public class githubServices {
+
+        private final String GITHUB_API_BASE_URL = "https://api.github.com";
+        private final RestTemplate restTemplate;
+
+        public githubServices(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        public Repository[] getUserRepositories(String username) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+            String apiUrl = GITHUB_API_BASE_URL + "/users/" + username + "/repos";
+
+            try {
+                return restTemplate.exchange(
+                        apiUrl,
+                        HttpMethod.GET,
+                        entity,
+                        Repository[].class
+                ).getBody();
+
+
+            } catch (HttpClientErrorException.NotFound notFoundException) {
+                return null;
+            }
+
+        }
+
+
+
+}
